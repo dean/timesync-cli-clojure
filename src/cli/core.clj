7e7af2203748edcf1c66d8ba8d0f3b7cb6c0ffef
Dean Johnson <deanjohnson222@gmail.com>
2015-04-21 15:11:09 -0700

    Few more additions.
    Still working to understand language and paradigms.

--- src/cli/core.clj
+++ src/cli/core.clj
@@ -4,23 +4,37 @@
   (:require [clj-http.lite.client :as client])
   (:gen-class))
 
+  ;; (some
+  ;;   (for [proj [seq get-avail-projects]
+  ;;     [#(= (get proj "name") to-find)]])))
+
 (defn get-avail-projects []
-  (let [url "timetracking.io/projects"]
-    (get (get (client/get url {:accept :json}) "data") "projects")))
+  (let [url "http://140.211.168.242/projects"]
+    [client/get url {:accept :json}]))
+
+
+(defn proj-in-list [to-find]
+  [get-avail-projects])
 
 (def cli-options
-  [["-p" "--project PROJECT" "Project Slug"]
-    ;; :validate #(contains? get-avail-projects %)]
+  [["-p" "--project PROJECT" "Project Slug"
+    :validate [#(proj-in-list %)]]
     ["-d" "--duration DURATION" "Duration (Minutes)"
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(> % 0) "Must be a number greater than 0."]]
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(> % 0) "Must be a number greater than 0."]]
     ["-u" "--username USERNAME" "Username to log time under."]
     ;; :validate ] This will later validate against the current users in the db.
-    ["-a" nil "Activity time spent working on."]
-    ["-n" nil "Notes about activity worked on."]
-    ["-i" nil "URI for issue being worked on."]])
+    ["-a" "--activity ACTIVITY" "Activity time spent working on."
+      :id :activity]
+    ["-n" "--notes NOTES" "Notes about activity worked on."
+      :id :notes]
+    ["-i" "--issue ISSUE" "URI for issue being worked on."
+      :id :issue]])
+
+;; (defn send_req []
+;;  )
 
 (defn -main [& args]
   ;; Comments/Docstrings?
-  (parse-opts args cli-options)
-  (println "Hello!"))
+  ;;(println (parse-opts args cli-options)))
+  (println (proj-in-list "orvsd") ))
